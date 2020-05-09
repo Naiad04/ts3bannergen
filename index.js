f0a00e1ed20c781692298a3bd8f99d14a3a3f55d
Naiad04 <alperenyurdakul7@gmail.com>
2020-05-09 17:46:24 +0300

    delete unnecessary comments

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // End image size should be 950x300 for best results in a 1920x1080 full screen
 
-//const SteamAPI = require('steamapi');
-//const steam = new SteamAPI('B1F2E9BA530F88ACC7B8E19C384C932E');
-//const steamIds = ["76561198101990975", "76561198213975395", "76561198073809055", "76561198128938401", "76561198122620661"];
 var popImageUrl = "./pics/emptyPopImage.png";
 
 const express = require('express');
